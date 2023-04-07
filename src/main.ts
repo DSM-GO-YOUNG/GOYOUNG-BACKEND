17068d32c720585f707bde61373fc7f2ea8f98d6
yebin <yebin5127@gmail.com>
2023-04-07 14:15:53 +0900

    Update main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import morgan from 'morgan';
+
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  app.use(morgan('dev'));
+  console.log(PORT, '번 포트에서 대기 중');
 }
 bootstrap();
