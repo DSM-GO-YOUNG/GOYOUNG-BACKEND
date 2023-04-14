0c725f6cb39429d7eae5d10de6150371f31fc6ec
yebin <yebin5127@gmail.com>
2023-04-14 14:09:14 +0900

    Update user service
    login 기능 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,15 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { User } from './schemas/user.schema';
-import { CreateUserDTO } from './dto/user.dto';
+import { CreateUserDTO, LoginUserDTO, UserResDTO } from './dto/user.dto';
 import { UserRepository } from './user.repository';
+import { comparePassword, generateHash } from 'src/shared/hash';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
     constructor(
-        private readonly userRepository: UserRepository
+        private readonly userRepository: UserRepository,
+        private readonly jwtService: JwtService
     ) {}
     
     public async signUp(createUserDto: CreateUserDTO): Promise<User> {
@@ -14,6 +17,24 @@ export class UserService {
         
         if(user) throw new ConflictException();
         
-        return await this.userRepository.createUser(createUserDto);
+        const hashedPassword = generateHash(createUserDto.password);
+        const newCreateUserDto = { ...createUserDto, password: hashedPassword };
+
+        return await this.userRepository.createUser(newCreateUserDto);
+    }
+
+    public async login(loginUserDto: LoginUserDTO): Promise<UserResDTO> {
+        const user = await this.userRepository.findUserByPhone(loginUserDto.phone);
+
+        if(!user) throw new NotFoundException('존재하지 않는 전화번호');
+
+        const isValid = comparePassword(user.password, loginUserDto.password);
+        if(!isValid) {
+            throw new UnauthorizedException();
+        }
+
+        const token = this.jwtService.sign({ id: user._id });
+        
+        return { access_token: token, _id: user._id };
     }
 }
