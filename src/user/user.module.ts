84b90b10924a79e56f46e3e8361bab68c70302ad
yebin <yebin5127@gmail.com>
2023-04-14 14:10:32 +0900

    Update user module
    jwt module 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,12 +4,21 @@ import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, userSchema } from './schemas/user.schema';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from "@nestjs/config";
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: User.name, schema: userSchema },
         ]),
+        JwtModule.registerAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                secret: config.get('JWT_KEY'),
+                signOptions: { expiresIn: config.get('TOKEN_TIME')}
+            })
+        })
     ],
     controllers: [UserController],
     providers: [UserService, UserRepository]
