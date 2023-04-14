ad56b009eb4cd3a216ab005cd804bbdb4aeccb38
yebin <yebin5127@gmail.com>
2023-04-14 14:08:57 +0900

    Update user repository
    id로 user 찾기 기능 추가

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { CreateUserDTO } from './dto/user.dto';
 
 @Injectable()
@@ -18,5 +18,9 @@ export class UserRepository {
     
     async findUserByPhone(phone: string): Promise<User> {
         return await this.userModel.findOne({ phone });
+    }
+
+    async findUserById(id: ObjectId): Promise<User> {
+        return await this.userModel.findOne({ _id: id });
     }
 }
