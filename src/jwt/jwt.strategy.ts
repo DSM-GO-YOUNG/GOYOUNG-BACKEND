bd1732299578d862cdc25257fc189cd8963b9224
yebin <yebin5127@gmail.com>
2023-04-14 14:07:58 +0900

    Create jwt strategy

---
+++ src/jwt/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UserRepository } from "src/user/user.repository";
+import { Payload } from "./jwt.payload";
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private readonly userRepository: UserRepository
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWT_KEY
+        });
+    }
+
+    async validate(payload: Payload) {
+        const user = await this.userRepository.findUserById(payload._id)
+
+        if(user) return user;
+        else throw new NotFoundException('사용자 정보 찾을 수 없음');
+    }
+}
