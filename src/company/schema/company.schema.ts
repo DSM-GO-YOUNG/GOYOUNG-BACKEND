9f34c0f0fa01221af67cb182deebdf1e8aa23c92
yebin <yebin5127@gmail.com>
2023-04-14 21:12:50 +0900

    Create company schema
    company 스키마 생성

---
+++ src/company/schema/company.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { ObjectId } from "mongoose";
+import { User } from "src/user/schema/user.schema";
+
+@Schema({ timestamps: true })
+export class Company {
+    _id: ObjectId;
+
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    kind: string;
+
+    @Prop({ required: true })
+    explanation: string;
+
+    @Prop({ required: true })
+    address: string;
+
+    @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: User.name })
+    user_id: User
+}
+
+export const companySchema = SchemaFactory.createForClass(Company);
