7228de32d5b89dfc97886de3de3043ab549d2d1d
yebin <yebin5127@gmail.com>
2023-04-14 14:05:26 +0900

    Update logging

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan("dev"));
+  app.enableCors();
   await app.listen(PORT);
-  app.use(morgan('dev'));
   console.log(PORT, '번 포트에서 대기 중');
 }
 bootstrap();
