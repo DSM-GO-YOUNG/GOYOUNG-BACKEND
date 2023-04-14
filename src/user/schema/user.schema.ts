a257a457d35163d0f6f60907d53820027fc2b24c
yebin <yebin5127@gmail.com>
2023-04-14 21:09:41 +0900

    Update user schema
    user 스키마 수정

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -5,6 +5,9 @@ import { ObjectId } from "mongoose";
 export class User {
     _id: ObjectId;
 
+    @Prop({ required: true, default: false })
+    host: boolean;
+
     @Prop({ required: true, trim: true })
     name: string;
 
@@ -13,6 +16,9 @@ export class User {
 
     @Prop({ required: true, trim: true, unique: true })
     phone: string;
+
+    @Prop({ required: false })
+    image: string;
 }
 
 export const userSchema = SchemaFactory.createForClass(User);
