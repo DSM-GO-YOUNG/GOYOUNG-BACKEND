120154e6e46e295caeae46401ce08dfed090e9cb
yebin <yebin5127@gmail.com>
2023-04-14 21:17:25 +0900

    Update app module
    auth, company 모듈 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from '../user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { CompanyModule } from 'src/company/company.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
     MongooseModule.forRoot(process.env.DB_URL),
+    AuthModule,
     UserModule,
+    CompanyModule
   ],
 })
 export class AppModule {}
