9e8804af988609f9ad3b202a7e348fc69891a480
yebin <yebin5127@gmail.com>
2023-04-14 14:07:41 +0900

    Update config module
    config module 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from '../user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.DB_URL),
     UserModule,
   ],
