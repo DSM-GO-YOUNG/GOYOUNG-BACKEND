eaa8b978977993fe8218edc989c1c21343e53561
yebin <yebin5127@gmail.com>
2023-04-17 09:13:21 +0900

    Create job-offer schema
    구인 공고 schema 설정

---
+++ src/job-offer/schema/job-offer.schema.ts
@@ -0,0 +1,41 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { ObjectId, mongo } from "mongoose";
+import { Company } from "src/company/schema/company.schema";
+import { User } from "src/user/schema/user.schema";
+
+@Schema({ timestamps: true })
+export class JobOffer {
+    _id: ObjectId;
+
+    @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: Company.name })
+    company_id: Company;
+
+    @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: User.name })
+    user_id: User;
+
+    @Prop({ required: true })
+    content: string;
+
+    @Prop({ required: true })
+    job: string;
+
+    @Prop({ required: true })
+    address: string;
+
+    @Prop({ required: true })
+    start_time: string;
+
+    @Prop({ required: true })
+    end_time: string;
+
+    @Prop({ required: false })
+    welfare: string;
+
+    @Prop({ required: true })
+    condition: string;
+
+    @Prop({ required: false })
+    prefer: string;
+}
+
+export const JobOfferSchema = SchemaFactory.createForClass(JobOffer);
