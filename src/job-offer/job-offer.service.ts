27bac7541c2320fd5d63489245dac516b1f54dbb
yebin <yebin5127@gmail.com>
2023-04-18 19:18:59 +0900

    Update job-offer service

--- src/job-offer/job-offer.service.ts
+++ src/job-offer/job-offer.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { JobOfferRepository } from './job.offer-repository';
+import { CreateJobOfferDTO } from './dto/job-offer.dto';
+import { JobOffer } from './schema/job-offer.schema';
 
 @Injectable()
-export class JobOfferService {}
+export class JobOfferService {
+    constructor(
+        private readonly jobOfferRepository: JobOfferRepository
+    ) {}
+
+    public async createJobOffer(createJobOfferDto: CreateJobOfferDTO): Promise<JobOffer> {
+        return 
+    }
+}
