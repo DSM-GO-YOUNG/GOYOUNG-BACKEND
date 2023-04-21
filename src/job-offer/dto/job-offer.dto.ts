69903cc00f1d3c50ab3af526ca518c03c9c81bf8
yebin <yebin5127@gmail.com>
2023-04-21 10:18:18 +0900

    Update job-offer dto
    job-offer dto 추가

--- src/job-offer/dto/job-offer.dto.ts
+++ src/job-offer/dto/job-offer.dto.ts
@@ -1,6 +1,39 @@
 import { IsString } from "class-validator";
+import { ObjectId } from "mongoose";
 
 export class CreateJobOfferDTO {
+    @IsString()
+    content: string;
+
+    @IsString()
+    job: string;
+
+    @IsString()
+    address: string;
+
+    @IsString()
+    start_time: string;
+
+    @IsString()
+    end_time: string;
+
+    @IsString()
+    welfare: string;
+
+    @IsString()
+    condition: string;
+
+    @IsString()
+    prefer: string;
+}
+
+export class JobOfferInfoDTO {
+    @IsString()
+    company_id: ObjectId;
+
+    @IsString()
+    user_id: ObjectId;
+
     @IsString()
     content: string;
 
