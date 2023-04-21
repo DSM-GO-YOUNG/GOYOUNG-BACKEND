9e99c9d3cd41175d6513ea3c8b7641edeb6eaa6f
yebin <yebin5127@gmail.com>
2023-04-21 10:20:13 +0900

    Update job-offer module
    의존성 주입

--- src/job-offer/job-offer.module.ts
+++ src/job-offer/job-offer.module.ts
@@ -4,14 +4,19 @@ import { JobOfferController } from './job-offer.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JobOffer, JobOfferSchema } from './schema/job-offer.schema';
 import { JobOfferRepository } from './job.offer-repository';
+import { CompanyRepository } from 'src/company/company.repository';
+import { Company, companySchema } from 'src/company/schema/company.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: JobOffer.name, schema: JobOfferSchema },
+    ]),
+    MongooseModule.forFeature([
+      { name: Company.name, schema: companySchema },
     ])
   ],
   controllers: [JobOfferController],
-  providers: [JobOfferService, JobOfferRepository]
+  providers: [JobOfferService, JobOfferRepository, CompanyRepository]
 })
 export class JobOfferModule {}
