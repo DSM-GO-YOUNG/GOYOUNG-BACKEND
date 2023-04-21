c570fe1f8237fbddfbcf345b2b224631064f0800
yebin <yebin5127@gmail.com>
2023-04-21 10:19:52 +0900

    Update job-offer controller
    구인공고 생성 기능 URL 설정

--- src/job-offer/job-offer.controller.ts
+++ src/job-offer/job-offer.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { JobOfferService } from './job-offer.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateJobOfferDTO } from './dto/job-offer.dto';
+import { Request } from 'express';
+import { User } from 'src/user/schema/user.schema';
+import { ObjectId } from 'mongoose';
 
 @Controller('job-offer')
 export class JobOfferController {
-  constructor(private readonly jobOfferService: JobOfferService) {}
+  constructor(
+    private readonly jobOfferService: JobOfferService
+    ) {}
+
+    @UseGuards(AuthGuard('jwt'))
+    @Post('/')
+    async createJobOffer(
+      @Body() createJobOfferDto: CreateJobOfferDTO,
+      @Req() req: Request,
+      @Param() company_id: ObjectId
+    ) {
+      await this.jobOfferService.createJobOffer(createJobOfferDto, req.user as User, company_id);
+
+    }
 }
