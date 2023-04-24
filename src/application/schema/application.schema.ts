6bfa791d2c7239ee448642c172237638720c377a
yebin <yebin5127@gmail.com>
2023-04-24 19:03:16 +0900

    Create: application schema 생성

---
+++ src/application/schema/application.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { ObjectId } from "mongoose";
+import { JobOffer } from "src/job-offer/schema/job-offer.schema";
+import { User } from "src/user/schema/user.schema";
+
+@Schema({ timestamps: true })
+export class Application {
+    _id: ObjectId;
+
+    @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: User.name })
+    user_id: User;
+
+    @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: JobOffer.name })
+    job_offer_id: JobOffer;
+
+    @Prop({ required: true })
+    content: string;
+}
+
+export const applicationSchema = SchemaFactory.createForClass(Application);
