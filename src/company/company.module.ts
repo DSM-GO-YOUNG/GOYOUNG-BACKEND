9e16693ab1ee0666cb81747b11a77eecc2120f74
yebin <yebin5127@gmail.com>
2023-05-19 20:23:54 +0900

    Update: multer모듈 import

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -4,12 +4,17 @@ import { CompanyController } from './company.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Company, companySchema } from './schema/company.schema';
 import { CompanyRepository } from './company.repository';
+import { MulterModule } from '@nestjs/platform-express';
+import { multerOptionsFactory } from 'src/shared/multerOptions';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Company.name, schema: companySchema },
-    ])
+    ]),
+    MulterModule.registerAsync({
+      useFactory: multerOptionsFactory
+    })
   ],
   providers: [CompanyService, CompanyRepository],
   controllers: [CompanyController]
