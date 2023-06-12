dd67cc45044a7ca0890a6b5f532e7b2c198b2795
yebin <yebin5127@gmail.com>
2023-06-12 14:19:05 +0900

    Update: multer 모듈 import

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,12 +4,17 @@ import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, userSchema } from './schema/user.schema';
 import { UserRepository } from './user.repository';
+import { MulterModule } from '@nestjs/platform-express';
+import { multerOptionsFactory } from 'src/shared/multerOptions';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: User.name, schema: userSchema },
-        ])
+        ]),
+        MulterModule.registerAsync({
+            useFactory: multerOptionsFactory
+          })
     ],
     controllers: [UserController],
     providers: [UserService, UserRepository]
